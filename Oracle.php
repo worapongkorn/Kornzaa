ade850437088759df9c70cc4907b84b6736ad5e0
worapongkorn <korn_naru_korn@hotmail.com>
2015-10-26 14:44:40 +0700

    HWFunDB

---
+++ Oracle.php
@@ -0,0 +1,61 @@
+<Strong>Login system with PHP</Strong>
+<hr>
+<ul>
+	<li>
+		<Strong>Enviroment machine</Strong>
+		<ul>
+			<li>Install database(Oracle)</li>
+			<li>Install web server(Apache)</li>
+			<li>Install php engine(pack in Apache)</li>
+			<li>e.g., <strong style='color:red'>XAMPP Web server</strong> or other</li>
+		</ul>
+	</li>
+	<li>
+		<Strong>Reference and Processes</Strong>
+		<ul>
+			<li>Set referance</li>
+			<li>Connected database</li>
+			<li>Read data(with conditions)</li>
+			<li>
+				Set global resource(or variable)
+				<ul>
+					<li>
+						<Strong>Session</Strong>
+						<ul>
+							<li>session_start()</li>
+							<li>$_SESSION['session_name']</li>
+							<li>unset($_SESSION['session_name'])</li>
+							<li>session_destroy()</li>
+						</ul>
+					</li>
+					<li>Cookie</li>
+				</ul>
+			</li>
+			<li>Set environment(for any user)</li>
+			<li>Set enviroment for any page</li>
+		</ul>
+	</li>
+	<li>
+		<strong>Login</strong>
+		<ul>
+			<li>
+				<Strong>Login form</Strong>
+				<ul>
+					<li>Input e.g, textbox for input Username and Password</li>
+					<li>Define execution state e.g, submit button</li>
+				</ul>
+			</li>
+			<li>
+				Logout form
+			</li>
+		</ul>
+	</li>
+	<li>
+		<Strong>Example</Strong>
+		<ul>
+			<li><a href='login.php'>Login form</a></li>
+			<li><a href='MemberPage.php'>Member page</a></li>
+			<li><a href='logout.php'>Logout</a></li>
+		</ul>
+	</li>
+</ul>
